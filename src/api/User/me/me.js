be3a08d92b588118209ee4ebcadf7f91d58ef6a5
정원호 <wonho1401@naver.com>
2020-09-16 15:16:15 +0900

    Fix fragments

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -1,5 +1,5 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { USER_FRAGMENT } from "../../fragments";
+import { USER_FRAGMENT } from "../../../fragments";
 
 export default {
   Query: {
