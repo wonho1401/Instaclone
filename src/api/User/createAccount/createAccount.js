4631caf5aadb5b8a0158656913e28b1d1415e7cd
정원호 <wonho1401@naver.com>
2020-09-24 15:36:21 +0900

    Change createAccount

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,14 +4,20 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { nickname, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
+      const exists = await prisma.$exists.user({
+        OR: [{ nickname }, { email }],
+      });
+      if (exists) {
+        throw Error("This nickname / email is already used");
+      }
+      await prisma.createUser({
         nickname,
         email,
         firstName,
         lastName,
         bio,
       });
-      return user;
+      return true;
     },
   },
 };
