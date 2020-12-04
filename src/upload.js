881e4214ece71fb37a7901fdfb43903927283c39
정원호 <wonho1401@naver.com>
2020-12-04 11:41:09 +0900

    upload image using aws-sdk, multer-s3

--- src/upload.js
+++ src/upload.js
@@ -1,11 +1,35 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
-const upload = multer({ dest: "uploads/" });
+//multer-s3 는 아마존 웹 서버에 파일들을 업로드할 수 있게 해줌.
+//aws-sdk는 aws를 이용하는데 사용하는듯(?)
+
+//s3라는 것을 만들어서 내 aws S3에 접근
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_SECRET,
+  region: "ap-northeast-2",
+});
+//multer를 이용해 이미지 업로드함. 저장장소를 설정하는것.
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: "w0noinsta",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
 
 export const uploadMiddleware = upload.single("file");
 export const uploadController = (req, res) => {
   const {
-    file: { path },
+    file: { location },
   } = req;
-  res.json({ path });
+  res.json({ location });
 };
